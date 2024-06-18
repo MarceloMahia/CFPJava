263244cce6ead165a48f86cf5cc3149d8a7c085f
MarceloMahia <115752120+MarceloMahia@users.noreply.github.com>
2024-06-17 23:16:19 -0300

    Primer commit de prueba

--- src/HorarioAsignado.java
+++ src/HorarioAsignado.java
@@ -1,2 +1,19 @@
-public class HorarioAsignado {
+import java.util.Scanner;
+
+public class HorarioAsignado
+{
+    Scanner scanner = new Scanner(System.in);
+
+    // Definimos el horario de la semana
+    String[][] horario = {
+            {"Matemáticas", "Ciencias", "Historia", "Educación Física", "Arte"}, // Lunes
+            {"Inglés", "Matemáticas", "Biología", "Química", "Historia"},         // Martes
+            {"Geografía", "Inglés", "Física", "Ciencias Sociales", "Educación Física"}, // Miércoles
+            {"Literatura", "Matemáticas", "Historia", "Ciencias", "Música"},      // Jueves
+            {"Computación", "Biología", "Química", "Matemáticas", "Educación Física"}   // Viernes
+    };
+
+
+
+
 }
